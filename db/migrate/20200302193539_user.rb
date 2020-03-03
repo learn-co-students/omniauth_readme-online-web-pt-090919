e0db1a7312c94242cf6ab21c090ae582ebb05a43
github username <github email address>
2020-03-03 09:22:27 -0600

    Done.

---
+++ db/migrate/20200302193539_user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Migration[5.0]
+  def change
+   create_table :users do |t|
+    t.string :name
+    t.string :email
+    t.string :image
+    t.integer :uid    
+  end
+end
